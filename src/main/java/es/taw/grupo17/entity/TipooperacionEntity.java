5adfddcd797f4b7897a5ac60683db82d5be754ce
aryands14 <aryands14702@gmail.com>
2023-03-13 14:27:56 +0100

    added entites

---
+++ src/main/java/es/taw/grupo17/entity/TipooperacionEntity.java
@@ -0,0 +1,46 @@
+package es.taw.grupo17.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tipooperacion", schema = "grupo17", catalog = "")
+public class TipooperacionEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "ID", nullable = false)
+    private int id;
+    @Basic
+    @Column(name = "DESCRIPCION", nullable = false, length = 45)
+    private String descripcion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TipooperacionEntity that = (TipooperacionEntity) o;
+        return id == that.id && Objects.equals(descripcion, that.descripcion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descripcion);
+    }
+}
