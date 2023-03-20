737d5c178076342b8f179b0b8c5b4142b851454a
aryands14 <aryands14702@gmail.com>
2023-03-20 13:30:55 +0100

    added doAutenticar in LoginController

--- src/main/java/es/taw/grupo17/controller/LoginController.java
+++ src/main/java/es/taw/grupo17/controller/LoginController.java
@@ -1,14 +1,40 @@
 package es.taw.grupo17.controller;
 
+import es.taw.grupo17.dao.EmpleadoRepository;
+import es.taw.grupo17.entity.EmpleadoEntity;
+import jakarta.persistence.Entity;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    protected EmpleadoRepository empleadoRepository;
+
     @GetMapping("/")
     public String doLogin() {
         return "login";
+    }
+
+    @PostMapping("/")
+    public String doAutenticar(@RequestParam("usuario") String user,
+                               @RequestParam("clave") String clave, Model model, HttpSession session) {
+        String urlTo = "redirect:/customer/";
+        EmpleadoEntity empleado = this.empleadoRepository.autenticar(user, clave);
+        if(empleado == null) {
+            model.addAttribute("error", "Credenciales incorrectas");
+            urlTo = "login";
+        } else {
+            session.setAttribute("empleado", empleado);
+        }
+        return urlTo;
     }
 
 
--- src/main/java/es/taw/grupo17/dao/EmpleadoRepository.java
+++ src/main/java/es/taw/grupo17/dao/EmpleadoRepository.java
@@ -3,6 +3,11 @@ package es.taw.grupo17.dao;
 import es.taw.grupo17.entity.EmpleadoEntity;
 import es.taw.grupo17.entity.PersonaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmpleadoRepository  extends JpaRepository<EmpleadoEntity, Integer> {
+    @Query("select e from EmpleadoEntity e where e.usuario = :username and e.contraseña = :password")
+    public EmpleadoEntity autenticar(@Param("username")String user, @Param("password") String password);
+
 }
